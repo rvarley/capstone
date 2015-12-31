2bff9cfcb0e79becb0f18bb0cee6c5dc914df467
ransomv <rvarley2001@yahoo.com>
2015-12-30 18:43:24 -0800

    removed default value for Price attribute

--- configurator/bike_app/models.py
+++ configurator/bike_app/models.py
@@ -3,9 +3,10 @@ from django.db import models
 
 class Bike(models.Model):
     Model = models.CharField(max_length=70)
-    Price = models.DecimalField(default=1501.00, max_digits=6, decimal_places=2)
+    # Price = models.DecimalField(default=1501.00, max_digits=6, decimal_places=2)
+    Price = models.DecimalField(max_digits=6, decimal_places=2)
     Best_Use = models.CharField(max_length=80, default="Commuter & Urban")
     Range = models.DecimalField(default=21, max_digits=4, decimal_places=0)
 
     def __unicode__(self):
-        return self.name
+        return self.Model
