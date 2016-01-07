6663b50850e9a422a3f21783422bb4382a33f34d
ransomv <rvarley2001@yahoo.com>
2016-01-06 18:59:08 -0800

    Changed field sizes, added fields for assistance, motor, top_speed, weight, brakes and battery

--- configurator/bike_app/models.py
+++ configurator/bike_app/models.py
@@ -3,11 +3,16 @@ from django.db import models
 
 class Bike(models.Model):
     model = models.CharField(max_length=70)
-    # Price = models.DecimalField(default=1501.00, max_digits=6, decimal_places=2)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     url = models.CharField(max_length=150)
-    best_use = models.CharField(max_length=80, default="Commuter & Urban")
-    b_range = models.CharField(max_length=20)
+    best_use = models.TextField(default="n/a")
+    b_range = models.CharField(max_length=6, default="n/a")
+    assistance = models.CharField(max_length=50, default="n/a")
+    motor = models.TextField(default="n/a")
+    top_speed = models.CharField(max_length=40, default="n/a")
+    weight = models.CharField(max_length=12, default="n/a")
+    brakes = models.TextField(default="n/a")
+    battery = models.TextField(default="n/a")
 
     def __unicode__(self):
         return self.model
