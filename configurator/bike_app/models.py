dd12f48970dc274fdd17f97b7ef53957e3d420c7
ransomv <rvarley2001@yahoo.com>
2016-01-08 14:12:09 -0800

    changed top_speed field from 40 to 6 chars.  Added photo field.

--- configurator/bike_app/models.py
+++ configurator/bike_app/models.py
@@ -6,13 +6,15 @@ class Bike(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     url = models.CharField(max_length=150)
     best_use = models.TextField(default="n/a")
+    photo = models.TextField(default="n/a")
     b_range = models.CharField(max_length=6, default="n/a")
     assistance = models.CharField(max_length=50, default="n/a")
     motor = models.TextField(default="n/a")
-    top_speed = models.CharField(max_length=40, default="n/a")
+    top_speed = models.CharField(max_length=6, default="n/a")
     weight = models.CharField(max_length=12, default="n/a")
     brakes = models.TextField(default="n/a")
     battery = models.TextField(default="n/a")
 
     def __unicode__(self):
-        return self.model
+        # return self.model
+        return '%s %s' % (self.model, self.best_use)
