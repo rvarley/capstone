952c13e76e4530ef301511f4865ad23b2aa3cfdd
ransomv <rvarley2001@yahoo.com>
2016-01-13 16:35:38 -0800

    added method for bike_details which will link to a detailed page for each bike model

--- configurator/bike_app/views.py
+++ configurator/bike_app/views.py
@@ -18,29 +18,14 @@ def home_page(request):
 
 def form_page(request):
 
-    #  The following populates the Bike database with values in output.txt file.
-    #  Uncomment to populate database with bike models
-    """
-    with open('output.txt', 'r') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
-        for row in spamreader:
-            for i, field in enumerate(row):
-                b = Bike(model=row[i])
-                b.save()
-
-    # bike_ = Bike.objects.all()  # returns list of Bike objects
-    # [x[1] for x in bike_.values_list()]  #Returns list of all bike models in table
-    """
-
     return render(request, 'form.html')
 
 
 def submit_response(request):
-    # bikes = Bike.objects.all()  # returns list of Bike objects
     selected_choice = request.POST['bikes']
     selected_use = request.POST['bikeUse']
-    selected_range = request.POST['bikeRange']
-    selected_speed = request.POST['bikeSpeed']
+    # selected_range = request.POST['bikeRange']
+    # selected_speed = request.POST['bikeSpeed']
     if len(re.findall(r'\d+', selected_choice)) == 2:  # user selects price range
         lower_val = (re.findall(r'\d+', selected_choice))[0]
         upper_val = (re.findall(r'\d+', selected_choice))[1]
@@ -49,11 +34,27 @@ def submit_response(request):
         upper_val = "10000"
 
     bikes = Bike.objects.filter(price__gte=lower_val).filter(price__lt=upper_val).filter(best_use__icontains=selected_use)  # returns list of Bike objects
-    # print("***** selected_range is: ", selected_range)
-    # print("***** selected_speed is: ", selected_speed)
-    # print("***** selected_use is: ", selected_use)
+
     # print("***** bikes:bikes is: ", {'bikes': bikes})
     for bike in bikes:
         print("***** bike.model is: ", bike.price)
     return render(request, 'results.html', {'bikes': bikes})
     # return HttpResponseRedirect(reverse('results'))
+
+
+def bike_details(request):
+    # bikes = Bike.objects.all()  # returns list of Bike objects
+    # selected_choice = request.POST['bikes']
+    # print("selected choice in bike_details is "), selected_choice
+    # selected_use = request.POST['bikeUse']
+    # bikey = request.session.get('url')
+    # print("!!! Bike Model: ", bike_model)
+    # print("!!! Bikey: ", bikey)
+    # bike_model = request.get("bike_model")
+    # print("bike_model is: ", bike_model)
+    bike_model = request.POST['bike_model']
+    print("!!! Bike Model:  ", bike_model)
+    bikes = Bike.objects.filter(model__icontains='Faraday')  # returns list of Bike objects
+    for bike in bikes:
+        print("***** bike.model in views.bike_deails is: ", bike.model)
+    return render(request, 'bike_details.html', {'bikes': bikes})
