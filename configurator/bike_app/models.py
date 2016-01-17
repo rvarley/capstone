0a1eb6575de96df68af7070f3c4eb9ae062bc1fa
ransomv <rvarley2001@yahoo.com>
2016-01-17 10:27:11 -0800

    changed b_range to decimal

--- configurator/bike_app/models.py
+++ configurator/bike_app/models.py
@@ -7,15 +7,14 @@ class Bike(models.Model):
     url = models.CharField(max_length=150)
     best_use = models.TextField(default="n/a")
     photo = models.TextField(default="n/a")
-    b_range = models.CharField(max_length=6, default="n/a")
+    b_range = models.DecimalField(max_digits=6, decimal_places=0, default=20)
     assistance = models.CharField(max_length=50, default="n/a")
     motor = models.TextField(default="n/a")
-    top_speed = models.CharField(max_length=6, default="n/a")
+    top_speed = models.CharField(max_length=7, default="n/a")
     weight = models.CharField(max_length=12, default="n/a")
     brakes = models.TextField(default="n/a")
     battery = models.TextField(default="n/a")
     comments = models.TextField(default="n/a")
 
     def __unicode__(self):
-        # return self.model
-        return '%s %s' % (self.model, self.best_use)
+        return self.model
