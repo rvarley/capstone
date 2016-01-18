88d88989accec22a3cbb73cd0f9d227cd040506d
ransomv <rvarley2001@yahoo.com>
2016-01-18 11:58:29 -0800

    Cleaned up per PEP8.  Got rid of un-necessary print statements.  Changed object returned in submit_response function from bikes to results.  This allows base template to know if a results object was returned and behave accordingly.

--- configurator/bike_app/views.py
+++ configurator/bike_app/views.py
@@ -4,7 +4,8 @@ from bike_app.models import Bike
 
 
 def home_page(request):
-    return render(request, 'home.html')
+    home = {"home": "home"}
+    return render(request, 'home.html', {'home': home})
 
 
 def form_page(request):
@@ -17,32 +18,25 @@ def submit_response(request):
     selected_use = request.POST['bikeUse']
     selected_range = request.POST['bikeRange']
     selected_speed = request.POST['bikeSpeed']
-    if len(re.findall(r'\d+', selected_choice)) == 2:  # user selects price range
+    if len(re.findall(r'\d+', selected_choice)) == 2:  # user selects a range
+                                                       # of prices
         lower_val = (re.findall(r'\d+', selected_choice))[0]
         upper_val = (re.findall(r'\d+', selected_choice))[1]
-    else:  # In case user selects a price of > 4500.
+    else:  # In case user selects a price of > 4500 (single price vs. range)
         lower_val = (re.findall(r'\d+', selected_choice))[0]
         upper_val = "10000"
 
-    bikes = Bike.objects.filter(price__gte=lower_val)\
+    results = Bike.objects.filter(price__gte=lower_val)\
         .filter(price__lte=upper_val).filter(best_use__icontains=selected_use)\
         .filter(top_speed__gte=selected_speed)\
         .filter(b_range__gte=selected_range)  # returns list of Bike objects
 
-    print("***** selected_use is:", selected_use)
-    print("***** lower val {}: ".format(lower_val))
-    print("***** upper val {}: ".format(upper_val))
-    print("***** selected_range {}: ".format(selected_range))
-    print("***** Type, selected_range is: ", type(selected_range))
-    # for bike in bikes:
-    #     print("***** bike.model is:", bike.model)
-    return render(request, 'results.html', {'bikes': bikes})
+    return render(request, 'results.html', {'results': results})
 
 
 def bike_details(request):
 
     bike_model = request.POST['bike_model']
-    print("!!! Bike Model:  ", bike_model)
-    bikes = Bike.objects.filter(model__icontains=bike_model)  # returns list of Bike objects
-    print("***** bikes type in views.bike_details is: ", type(bikes))
+    bikes = Bike.objects.filter(model__icontains=bike_model)  # returns list
+                                                              # of Bike objects
     return render(request, 'bike_details.html', {'bikes': bikes})
