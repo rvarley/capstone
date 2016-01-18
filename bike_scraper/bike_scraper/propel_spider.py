9baf70a6c920aa7a5affc65285c7ef2fb239d35e
ransomv <rvarley2001@yahoo.com>
2016-01-18 11:26:24 -0800

    Chained commands that scrape and format price.  got rid of print statements. Added comments and broke up lines per PEP8 requirements

--- bike_scraper/bike_scraper/propel_spider.py
+++ bike_scraper/bike_scraper/propel_spider.py
@@ -27,13 +27,11 @@ class PropelSpider(scrapy.Spider):
     def parse(self, response):
         models = response.xpath("//div[@class='name']/a/text()").extract()
         prices = response.xpath("//span[@class='price-fixed']/text()").extract()
-        locale.setlocale(locale.LC_ALL, '')
+        locale.setlocale(locale.LC_ALL, '')  # Convert unicode currency to decimal
         urls = response.xpath("//div[@class='name']/a/@href").extract()
         for i in range(len(models)):
             b = BikeItem()
-            price = prices[i].strip('$')
-            price = price.replace(',', '')
-            price = "%.2f" % locale.atof(price[:])
+            price = "%.2f" % locale.atof(prices[i].strip('$').replace(',', '')[:])
             b['model'] = models[i]
             b['price'] = price
             b['url'] = urls[i]
@@ -43,7 +41,6 @@ class PropelSpider(scrapy.Spider):
                 check_bike = UpdateBike()
                 detail_changes = check_bike.update_bike(bike_record, b)  # bike
                 # record exists.  Updating price or url if changed
-                # print "!!! Bike exists, doing nothing !!!"
                 print "Value of detail_changes is:  " + detail_changes
                 continue
             b.save()
@@ -59,11 +56,14 @@ class PropelSpider(scrapy.Spider):
         spec = SpecsItem()
         Url = response.url
         print "value of Url in parse_attr is:  " + Url
-        spec["attr"] = response.xpath('//*[@id="tab-attribute"]/table/tbody/tr/td/text()').extract()
-        bike_dic = dict(map(None, *[iter(spec["attr"])]*2))  # Converts list spec["attr"] to dict
+        spec["attr"] = response.xpath('//*[@id="tab-attribute"]/table/tbody/tr\
+            /td/text()').extract()
+        bike_dic = dict(map(None, *[iter(spec["attr"])]*2))  
+        # Converts list spec["attr"] to dict
         b = Bike.objects.get(url=Url)
         print "Value of b.url is: " + b.url
-        b_range = filter(str.isdigit, bike_dic["Range"].encode('ascii', 'ignore'))
+        b_range = filter(str.isdigit, bike_dic["Range"]\
+            .encode('ascii', 'ignore'))
         best_use = bike_dic["Best Use"]
         assistance = bike_dic["Assistance"]
         motor = bike_dic["Motor"]
@@ -75,15 +75,10 @@ class PropelSpider(scrapy.Spider):
         brakes = bike_dic["Brakes"].rstrip('\r+\n')
         battery = bike_dic["Battery"]
         battery = battery.replace(',', '')  # get rid of any #'s with commas
-
-        # battery = re.findall(r'\d*\.?\d+[A,a,V,v,W,w,WH,Wh,wH,wh]+', battery)
         battery = re.findall(r'\d*\.?\d+\s*[A,a,V,v,WH,Wh,wH,wh]+', battery)
-        # if len(battery) > 3:  # Only save info on standard battery in database
         bat = []
         print " !!! Value of battery is  !!!", battery
         for i in range(len(battery)):
-            print "!!! Value of i is ", i
-            print "!!! Value of battery[i] is ", battery[i]
             if (i > 0) & ('v' in battery[i].lower()):  # save only 1st set of
                 break                                  # battery values
             bat.append(battery[i])
@@ -95,17 +90,19 @@ class PropelSpider(scrapy.Spider):
         b.top_speed = top_speed
         b.brakes = brakes
         if battery:  # Only populate field if relevent info exists
-            # battery = re.findall(r'\d*\.?\d+[A,a,V,v,WH,Wh,wH,wh]+', battery)
             battery = [str(x) for x in battery]  # conv unicode to ascii
             battery = [x.replace(' ', '') for x in battery]  # get rid of spaces
             b.battery = ' '.join(battery).upper()  # convert list to string
-        b.photo = response.xpath('//*[@id="content"]/div[3]/div[1]/div[1]/div[1]/a/img/@src').extract()
-        # These if's are necessary for 4 cases which xpath to photos isn't
-        # in the usual place.
+        b.photo = response.xpath('//*[@id="content"]/div[3]/div[1]/div[1]/\
+            div[1]/a/img/@src').extract()
+        # These if's are necessary for the 4 cases which xpath to photos isn't
+        # in the expected location
         if not b.photo:
-            b.photo = response.xpath('//*[@id="content"]/div[4]/div[1]/div[1]/div/a/img/@src').extract()
+            b.photo = response.xpath('//*[@id="content"]/div[4]/div[1]/div[1]\
+                /div/a/img/@src').extract()
             if not b.photo:
-                b.photo = response.xpath('//*[@id="content"]/div/div[1]/div[1]/div/a/@href').extract()
+                b.photo = response.xpath('//*[@id="content"]/div/div[1]/div[1]\
+                    /div/a/@href').extract()
                 print "!!! b.photo and Url for felt bike is: "
                 print b.photo
                 print Url
@@ -114,18 +111,14 @@ class PropelSpider(scrapy.Spider):
 
 
 class UpdateBike():
+
     def update_bike(self, bike_record, b):
-        print 'in update_or_add, bike_record.price is:'
-        print bike_record.price
-        print "in update_or_add, b['price'] is:"
-        print b['price']
-        print b['model']
         if bike_record.price != b['price']:
             print "!!!! price has changed.  updating price !!!!"
             bike_record.price = b['price']
             bike_record.save()
         if bike_record.url != b['url']:
-            print "!!!! price has changed.  updating price !!!!"
+            print "!!!! url has changed.  updating url !!!!"
             bike_record.url = b['url']
             bike_record.save()
         return 'done with update_or_add'
